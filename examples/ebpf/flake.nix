e1aaf08e5773304ccab880fd389da24d552b4d76
Benedikt Spies <benedikt.spies@gmail.com>
2026-03-14 02:08:30 +0100

    add ebpf example

---
+++ examples/ebpf/flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixnet.url = "github:birneee/nixnet";
+    starlink.url = "github:birneee/simple-starlink-ebpf";
+  };
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      nixnet = inputs.nixnet.legacyPackages.${system};
+      starlink = inputs.starlink.packages.${system}.default;
+    in
+    {
+      packages.${system}.default = nixnet.mkTestbed {
+        packages = with pkgs; [
+          coreutils
+          iperf3
+        ];
+        namespaces = {
+          client.scripts = [
+            {
+              exec = "sleep 0.1; iperf3 -c 10.0.0.2 -t 30 --forceflush";
+              await = true;
+            }
+          ];
+          server.scripts = [
+            {
+              exec = "iperf3 -s";
+            }
+          ];
+        };
+        links.veth0 = {
+          netem.delayMs = 40;
+          a = {
+            ns = "client";
+            ipv4 = "10.0.0.1/24";
+          };
+          b = {
+            ns = "server";
+            ipv4 = "10.0.0.2/24";
+          };
+        };
+        postSetup = ''
+          ip netns exec client ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp
+          ip netns exec server ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp
+        '';
+      };
+    };
+}
